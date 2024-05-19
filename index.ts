3945abcdcb5e090d57d7d0639e4158d7e71f27ca
d2kx <dennis@dmh.dev>
2024-05-20 00:25:59 +0200

    check ip for validity

--- index.ts
+++ index.ts
@@ -1,10 +1,20 @@
 import express, { Express, Request, Response } from 'express';
+import { isIP } from 'net';
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('please hire me');
+  res.send('please hire me <3');
+});
+
+app.get('/api/location/:ipAddress', (req: Request, res: Response) => {
+  const ipAddress = req.params.ipAddress;
+  if (!isIP(ipAddress)) {
+    res.send('please submit a valid IPv4/IPv6 address');
+    return;
+  }
+  res.send(`your ip: ${JSON.stringify(req.params)}`);
 });
 
 app.listen(port, () => {
