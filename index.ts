0a8cae88e5e022f1e5d68ac873c369fdcdeba9ea
d2kx <dennis@dmh.dev>
2024-05-20 01:36:23 +0200

    mostly? finish up the basic ip geolocation route

--- index.ts
+++ index.ts
@@ -1,20 +1,40 @@
 import express, { Express, Request, Response } from 'express';
 import { isIP } from 'net';
 
+interface IPGeolocationAPIResponse {
+  city: string;
+  country: string;
+  query: string;
+  status: string;
+}
+
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
+  // please do <3
   res.send('please hire me <3');
 });
 
-app.get('/api/location/:ipAddress', (req: Request, res: Response) => {
-  const ipAddress = req.params.ipAddress;
+app.get('/api/location/:ipAddress', async (req: Request, res: Response) => {
+  const { ipAddress } = req.params;
+
+  // don't even bother if we didn't get a valid IPv4/IPv6 address, both of which ip-api.com supports
   if (!isIP(ipAddress)) {
-    res.send('please submit a valid IPv4/IPv6 address');
+    res.status(400).send('please submit a valid IPv4/IPv6 address');
     return;
   }
-  res.send(`your ip: ${JSON.stringify(req.params)}`);
+
+  try {
+    // native fetch requires Node.js 16+, it became stable in 21+
+    const response = await fetch(`http://ip-api.com/json/${ipAddress}`);
+    const { city, country, query, status } =
+      (await response.json()) as IPGeolocationAPIResponse;
+
+    res.json({ city, country, ipAddress: query, status });
+  } catch (error) {
+    res.status(500).send((error as Error).message);
+  }
 });
 
 app.listen(port, () => {
