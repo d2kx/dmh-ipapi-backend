4674a0ff8da854f702ffb8376a042dce7926f87d
d2kx <dennis@dmh.dev>
2024-05-21 02:32:00 +0200

    some minor response updates

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ import { isIP } from 'net';
 interface IPGeolocationAPIResponse {
   city: string;
   country: string;
+  message?: string;
   query: string;
   status: string;
 }
@@ -26,19 +27,30 @@ app.get('/api/location/:ipAddress', async (req: Request, res: Response) => {
 
   // don't even bother if we didn't get a valid IPv4/IPv6 address, both of which ip-api.com supports
   if (!isIP(ipAddress)) {
-    res.status(400).send('please submit a valid IPv4/IPv6 address');
+    res.status(400).json({
+      status: 'error',
+      message: 'please submit a valid IPv4/IPv6 address',
+    });
     return;
   }
 
   try {
     // native fetch requires Node.js 16+, it became stable in 21+
     const response = await fetch(IP_GEOLOCATION_API_URL + ipAddress);
-    const { city, country, query, status } =
+
+    // ip-api.com only returns field 'message' on failure
+    const { city, country, message, query, status } =
       (await response.json()) as IPGeolocationAPIResponse;
 
-    res.json({ city, country, ipAddress: query, status });
+    // ip-api.com will return status 200 even in failure cases of e.g. reserved ranges like 127.0.0.1
+    // let's make our HTTP status dependend on its outcome instead
+    res
+      .status(status === 'success' ? 200 : 400)
+      .json({ city, country, ipAddress: query, message, status });
   } catch (error) {
-    res.status(500).send((error as Error).message);
+    res
+      .status(500)
+      .json({ status: 'error', message: (error as Error).message });
   }
 });
 
